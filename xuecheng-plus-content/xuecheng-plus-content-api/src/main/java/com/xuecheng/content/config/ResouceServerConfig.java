d28003eef8ff0aca3aaba812d4c2d5d1a7d679a6
will <767572558@qq.com>
2023-03-07 19:35:11 +0800

    测试资源服务校验令牌

---
+++ xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/config/ResouceServerConfig.java
@@ -0,0 +1,44 @@
+package com.xuecheng.content.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+
+/**
+ * @author Administrator
+ * @version 1.0
+ **/
+@Configuration
+@EnableResourceServer
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
+public class ResouceServerConfig extends ResourceServerConfigurerAdapter {
+
+    //资源服务标识(与AutoorizationServer中的client.resourceIds一致)
+    public static final String RESOURCE_ID = "xuecheng-plus";
+
+    @Autowired
+    TokenStore tokenStore;
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) {
+        resources.resourceId(RESOURCE_ID)//资源 id
+                .tokenStore(tokenStore)
+                .stateless(true);
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                //所有/r/**和/course/**的请求必须认证通过，即必须携带JWT令牌才能访问
+                .antMatchers("/r/**", "/course/**").authenticated()
+                .anyRequest().permitAll()
+        ;
+    }
+
+}
