32fd08467392d20d7d0760a63053db39265f32d0
will <767572558@qq.com>
2023-03-10 13:22:21 +0800

    auth微信扫码登录保存用户信息编写与测试（微信扫码登录功能完成！）

--- xuecheng-plus-auth/src/main/java/com/xuecheng/auth/controller/WxLoginController.java
+++ xuecheng-plus-auth/src/main/java/com/xuecheng/auth/controller/WxLoginController.java
@@ -26,19 +26,14 @@ public class WxLoginController {
     public String wxLogin(String code, String state) throws IOException {
         log.debug("微信扫码回调,code:{},state:{}", code, state);
         //1.远程调用微信申请令牌; 2.拿到令牌查询用户信息; 3.将用户信息写入本项目数据库
-        XcUser xcUser1 = wxAuthService.wxAuth(code);
+        XcUser xcUser = wxAuthService.wxAuth(code);
 
-        //暂时硬编写，便于调试环境
-        XcUser xcUser = new XcUser();
-        xcUser.setUsername("t1");
-        //XcUser xcUser = wxAuthService.wxAuth(code);
         if (xcUser == null) {
             return "redirect:http://www.xuecheng-plus.com/error.html";
         }
 
         String username = xcUser.getUsername();
-
-        //用户登录完成，直接重定向
+        //用户登录完成，直接重定向；重定向后进入统一认证入口，执行相应的execute方法
         return "redirect:http://www.xuecheng-plus.com/sign.html?username=" + username + "&authType=wx";
     }
 
--- xuecheng-plus-auth/src/main/java/com/xuecheng/ucenter/service/impl/WxAuthServiceImpl.java
+++ xuecheng-plus-auth/src/main/java/com/xuecheng/ucenter/service/impl/WxAuthServiceImpl.java
@@ -3,9 +3,11 @@ package com.xuecheng.ucenter.service.impl;
 import com.alibaba.fastjson.JSON;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.xuecheng.ucenter.mapper.XcUserMapper;
+import com.xuecheng.ucenter.mapper.XcUserRoleMapper;
 import com.xuecheng.ucenter.model.dto.AuthParamsDto;
 import com.xuecheng.ucenter.model.dto.XcUserExt;
 import com.xuecheng.ucenter.model.po.XcUser;
+import com.xuecheng.ucenter.model.po.XcUserRole;
 import com.xuecheng.ucenter.service.AuthService;
 import com.xuecheng.ucenter.service.WxAuthService;
 import lombok.extern.slf4j.Slf4j;
@@ -15,10 +17,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
 
 import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
 import java.util.Map;
+import java.util.UUID;
 
 /**
  * @author will
@@ -48,6 +53,12 @@ public class WxAuthServiceImpl implements AuthService, WxAuthService {
     @Autowired
     XcUserMapper xcUserMapper;
 
+    @Autowired
+    XcUserRoleMapper xcUserRoleMapper;
+
+    @Autowired
+    WxAuthServiceImpl currentProxy;
+
 
     /**
      * @param code 授权码
@@ -58,13 +69,13 @@ public class WxAuthServiceImpl implements AuthService, WxAuthService {
      */
     @Override
     public XcUser wxAuth(String code) {
-        //1. 收到code调用微信接口申请access_token
+        //1. 收到code调用微信接口申请access_token令牌
         Map<String, String> access_token_map = getAccess_token(code);
         if (access_token_map == null) {
             return null;
         }
 
-        //2. 使用access_token查询用户信息
+        //2. 使用access_token令牌查询用户信息
         String openid = access_token_map.get("openid");
         String access_token = access_token_map.get("access_token");
         Map<String, String> userinfo = getUserinfo(access_token, openid);
@@ -72,8 +83,8 @@ public class WxAuthServiceImpl implements AuthService, WxAuthService {
             return null;
         }
 
-        //3. 添加用户到数据库
-        XcUser xcUser = null;
+        //3. 添加用户信息到数据库
+        XcUser xcUser = currentProxy.addWxUser(userinfo);
         return xcUser;
     }
 
@@ -180,5 +191,48 @@ public class WxAuthServiceImpl implements AuthService, WxAuthService {
         return resultMap;
     }
 
+
+    /**
+     * @param userInfo_map map格式用户信息
+     * @return com.xuecheng.ucenter.model.po.XcUser
+     * @description 保存用户信息
+     * @author will
+     * @date 2023/3/10 13:11
+     */
+    @Transactional
+    public XcUser addWxUser(Map userInfo_map) {
+        String unionid = userInfo_map.get("unionid").toString();
+        //根据unionid查询数据库
+        XcUser xcUser = xcUserMapper.selectOne(new LambdaQueryWrapper<XcUser>().eq(XcUser::getWxUnionid, unionid));
+        if (xcUser != null) {
+            //该微信用户已注册
+            return xcUser;
+        }
+
+        //向用户基本信息表xc_user新增记录
+        xcUser = new XcUser();
+        xcUser.setId(UUID.randomUUID().toString()); //主键
+        xcUser.setUsername(unionid);
+        xcUser.setWxUnionid(unionid);
+        //记录从微信得到的昵称
+        xcUser.setNickname(userInfo_map.get("nickname").toString());
+        xcUser.setUserpic(userInfo_map.get("headimgurl").toString());
+        xcUser.setName(userInfo_map.get("nickname").toString());
+        xcUser.setPassword(unionid);
+        xcUser.setUtype("101001");  //学生类型
+        xcUser.setStatus("1");  //用户状态
+        xcUser.setCreateTime(LocalDateTime.now());
+        xcUserMapper.insert(xcUser);
+
+        //向用户-角色关系表xc_user_role新增记录
+        XcUserRole xcUserRole = new XcUserRole();
+        xcUserRole.setId(UUID.randomUUID().toString()); //主键
+        xcUserRole.setUserId(xcUser.getId()); //账号id
+        xcUserRole.setRoleId("17"); //学生角色（查看xc_role表）
+        xcUserRole.setCreateTime(LocalDateTime.now());
+        xcUserRoleMapper.insert(xcUserRole);
+
+        return xcUser;
+    }
 
 }
