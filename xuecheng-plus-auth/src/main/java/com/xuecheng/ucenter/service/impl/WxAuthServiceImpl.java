f7d6ce6d5381089da062d11dcc0fb514b43092da
will <767572558@qq.com>
2023-03-10 12:30:05 +0800

    auth微信扫码登录携带令牌获取用户信息编写与测试

--- xuecheng-plus-auth/src/main/java/com/xuecheng/ucenter/service/impl/WxAuthServiceImpl.java
+++ xuecheng-plus-auth/src/main/java/com/xuecheng/ucenter/service/impl/WxAuthServiceImpl.java
@@ -17,6 +17,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
 /**
@@ -49,23 +50,29 @@ public class WxAuthServiceImpl implements AuthService, WxAuthService {
 
 
     /**
-     * @param code  授权码
+     * @param code 授权码
      * @return com.xuecheng.ucenter.model.po.XcUser
-     * @description TODO
+     * @description 1.申请令牌，2.携带令牌查询用户信息，3.保存用户信息到数据库
      * @author will
      * @date 2023/3/10 11:58
      */
     @Override
     public XcUser wxAuth(String code) {
-        //收到code调用微信接口申请access_token
+        //1. 收到code调用微信接口申请access_token
         Map<String, String> access_token_map = getAccess_token(code);
         if (access_token_map == null) {
             return null;
         }
 
-        //获取用户信息
+        //2. 使用access_token查询用户信息
+        String openid = access_token_map.get("openid");
+        String access_token = access_token_map.get("access_token");
+        Map<String, String> userinfo = getUserinfo(access_token, openid);
+        if (userinfo == null) {
+            return null;
+        }
 
-        //添加用户到数据库
+        //3. 添加用户到数据库
         XcUser xcUser = null;
         return xcUser;
     }
@@ -130,4 +137,48 @@ public class WxAuthServiceImpl implements AuthService, WxAuthService {
         //map返回
         return resultMap;
     }
+
+
+    /**
+     * 获取用户信息，示例如下：
+     * {
+     * "openid":"OPENID",
+     * "nickname":"NICKNAME",
+     * "sex":1,
+     * "province":"PROVINCE",
+     * "city":"CITY",
+     * "country":"COUNTRY",
+     * "headimgurl": "https://thirdwx.qlogo.cn/mmopen/g3MonUZtNHkdmzicIlibx6iaFqAc56vxLSUfpb6n5WKSYVY0ChQKkiaJSgQ1dZuTOgvLLrhJbERQQ4eMsv84eavHiaiceqxibJxCfHe/0",
+     * "privilege":[
+     * "PRIVILEGE1",
+     * "PRIVILEGE2"
+     * ],
+     * "unionid": " o6_bmasdasdsad6_2sgVt7hMZOPfL"
+     * }
+     */
+    /**
+     * @param access_token 调用凭证/令牌
+     * @param openid       普通用户的标识，对当前开发者帐号唯一
+     * @return java.util.Map<java.lang.String, java.lang.String>
+     * @description 携带令牌访问用户信息
+     * @author will
+     * @date 2023/3/10 12:17
+     */
+    private Map<String, String> getUserinfo(String access_token, String openid) {
+        String wxUrl_template = "https://api.weixin.qq.com/sns/userinfo?access_token=%s&openid=%s";
+        //请求微信地址拼接，最终路径
+        String wxUrl = String.format(wxUrl_template, access_token, openid);
+        log.info("调用微信接口申请access_token, url:{}", wxUrl);
+        //远程调用此url
+        ResponseEntity<String> exchange = restTemplate.exchange(wxUrl, HttpMethod.POST, null, String.class);
+        //获取响应结果（上面的响应示例，json数据）额外处理nickname(普通用户昵称)乱码问题
+        String result = new String((exchange.getBody().getBytes(StandardCharsets.ISO_8859_1)), StandardCharsets.UTF_8);
+        log.info("调用微信接口申请access_token: 返回值:{}", result);
+        //将响应结果转成map
+        Map<String, String> resultMap = JSON.parseObject(result, Map.class);
+        //map返回
+        return resultMap;
+    }
+
+
 }
