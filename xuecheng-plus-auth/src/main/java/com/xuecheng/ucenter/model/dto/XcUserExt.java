a011b088b960a86a903d9e17b6cad0af5282e5d1
will <767572558@qq.com>
2023-03-11 15:14:44 +0800

    auth授权功能完成及测试

--- xuecheng-plus-auth/src/main/java/com/xuecheng/ucenter/model/dto/XcUserExt.java
+++ xuecheng-plus-auth/src/main/java/com/xuecheng/ucenter/model/dto/XcUserExt.java
@@ -7,10 +7,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * @description 用户扩展信息
  * @author Mr.M
- * @date 2022/9/30 13:56
  * @version 1.0
+ * @description 用户扩展信息
+ * @date 2022/9/30 13:56
  */
 @Data
 public class XcUserExt extends XcUser {
