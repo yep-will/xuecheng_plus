cc3644e7d594c2263ddcf80c6135c2a43fea063f
will <767572558@qq.com>
2023-03-21 12:54:15 +0800

    long转string避免精度损失配置

--- xuecheng-plus-base/src/main/java/com/xuecheng/base/config/LocalDateTimeConfig.java
+++ xuecheng-plus-base/src/main/java/com/xuecheng/base/config/LocalDateTimeConfig.java
@@ -1,10 +1,16 @@
 package com.xuecheng.base.config;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -40,6 +46,26 @@ public class LocalDateTimeConfig {
     @Bean
     public LocalDateTimeDeserializer localDateTimeDeserializer() {
         return new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+    }
+
+
+    /**
+     * @param builder
+     * @return com.fasterxml.jackson.databind.ObjectMapper
+     * @description long转string避免精度损失
+     * @author will
+     * @date 2023/3/21 12:48
+     */
+    @Bean
+    public ObjectMapper jacksonObjectMapper(Jackson2ObjectMapperBuilder builder) {
+        ObjectMapper objectMapper = builder.createXmlMapper(false).build();
+        //忽略value为null 时 key的输出
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        SimpleModule module = new SimpleModule();
+        module.addSerializer(Long.class, ToStringSerializer.instance);
+        module.addSerializer(Long.TYPE, ToStringSerializer.instance);
+        objectMapper.registerModule(module);
+        return objectMapper;
     }
 
 
